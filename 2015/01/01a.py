8bad3a40a1e1e15f388d0894eec5daa389e3fe49
Wodorek <Krol.w94@gmail.com>
2023-11-07 19:28:33 +0100

    2015 part 1

--- 2015/01/01a.py
+++ 2015/01/01a.py
@@ -1,3 +1,18 @@
 from helpers import get_input as gi
+from pathlib import Path
 
-gi.get_input('pacz')
+path = Path.cwd()
+
+input = gi.get_input(2015, 1, path)
+
+split = list(input)
+
+curr_floor = 0
+
+for el in split:
+    if el == '(':
+        curr_floor += 1
+    else:
+        curr_floor -= 1
+
+print('solution:', curr_floor)
