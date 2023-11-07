7bb83c84ae76f6407495193b1d7947cb5cfe17f5
Wodorek <Krol.w94@gmail.com>
2023-11-07 19:43:49 +0100

    Update get_input.py
    More ease of use

--- helpers/get_input.py
+++ helpers/get_input.py
@@ -1,17 +1,26 @@
 import os
 import requests
 from dotenv import load_dotenv
+from pathlib import Path
+
 
 load_dotenv()
 
 
-def get_input(year, day, path):
+def get_input():
+
+    path = Path.cwd()
+
+    path_string = path.as_posix().split('/')
+
     file_path = f'{path}\\input.txt'
     input_txt = open(file_path, "a+")
 
     if os.stat(input_txt.name).st_size == 0:
         print('downloading input')
-        url = f'https://adventofcode.com/{year}/day/{day}/input'
+        url = f'https://adventofcode.com/{path_string[-2]}/day/{path_string[-1].lstrip("0")}/input'
+
+        print(url)
         session_id = os.getenv('SESSION_ID')
         resp = requests.get(
             url, cookies={"session": session_id})  # type: ignore
