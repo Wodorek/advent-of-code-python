5d9dd6c8bf422b67ab634d8cf9f04f7cd133a338
Wodorek <Krol.w94@gmail.com>
2023-11-07 20:08:36 +0100

    2015 day 2 part 2

---
+++ 2015/02/02b.py
@@ -0,0 +1,24 @@
+from helpers import get_input as gi
+
+input = gi.get_input().splitlines()
+
+
+def calculate_paper(l: int, w: int, h: int):
+    bow = l*w*h
+
+    sorted_vals = sorted([l, w, h])
+
+    print(sorted_vals)
+
+    return bow + (sorted_vals[0] * 2) + (sorted_vals[1] * 2)
+
+
+total_feet = 0
+
+for present in input:
+    [l, w, h] = map(int, present.split('x'))
+
+    total_feet += calculate_paper(l, w, h)
+
+
+print('solution is:', total_feet)
