fd7bc69825b4355da64ada53d7ba25f2e3468e48
Wodorek <Krol.w94@gmail.com>
2023-11-07 19:05:57 +0100

    Imput getter func ready

--- helpers/get_input.py
+++ helpers/get_input.py
@@ -1,2 +1,22 @@
-def get_input(path):
-    print(path)
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def get_input(year, day, path):
+    file_path = f'{path}\\input.txt'
+    input_txt = open(file_path, "a+")
+
+    if os.stat(input_txt.name).st_size == 0:
+        print('downloading input')
+        url = f'https://adventofcode.com/{year}/day/{day}/input'
+        session_id = os.getenv('SESSION_ID')
+        resp = requests.get(
+            url, cookies={"session": session_id})  # type: ignore
+        print(resp.text)
+        input_txt.write(resp.text)
+
+    input_txt.seek(0)
+    return input_txt.read()
