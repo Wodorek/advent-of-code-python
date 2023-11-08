0ba69f1cd0e0506a4fa9434a5f6ec5c6cefaae21
Wodorek <Krol.w94@gmail.com>
2023-11-08 14:35:25 +0100

    2015 day 4 part 2

---
+++ 2015/04/04b.py
@@ -0,0 +1,26 @@
+from helpers import get_input as gi
+import hashlib
+
+input = gi.get_input().strip()
+
+
+def find_hash(num):
+
+    hash_str = f'{input}{num}'
+    hashed = hashlib.md5(hash_str.encode('utf-8'))
+
+    return hashed.hexdigest()
+
+
+found_hash = ''
+curr_idx = 0
+
+while True:
+    if found_hash[0:6] == '000000':
+        break
+    else:
+        curr_idx += 1
+        found_hash = find_hash(curr_idx)
+
+
+print(curr_idx)
